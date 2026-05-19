71c79411af57e5641d6e5fbaaaefb71d8abac039
Alisson G Costa <alisson.costa84200@gmail.com>
2026-05-19 19:15:04 -0300

    iniciando o spring security

--- src/main/java/com/costa/projetoBiblioteca/config/SecurityConfig1.java
+++ src/main/java/com/costa/projetoBiblioteca/config/SecurityConfig1.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig1 {
 
-///    @Bean
+   @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
